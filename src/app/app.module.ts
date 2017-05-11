5c991928fad2e14cf5ce3a58e8713a3abab9e462
cinexin <migueguillamon@gmail.com>
2017-05-11 19:29:57 +0200

    AngularJS.Level 1 solution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import {AppRacingComponent} from './appracing.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, AppRacingComponent ],
+  bootstrap:    [ AppComponent, AppRacingComponent ]
 })
 export class AppModule { }
