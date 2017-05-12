4f3219458e2eb66138965c55a34a75c152eb8101
cinexin <migueguillamon@gmail.com>
2017-05-12 13:05:45 +0200

    2-way data binding.Assigning properties to input boxes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import {AppRacingComponent} from './appracing.component';
@@ -7,7 +8,7 @@ import {CarPartsComponent} from './car-parts.component';
 import {MyRacesComponent} from './my.races.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, AppRacingComponent, CarPartsComponent, MyRacesComponent ],
   bootstrap:    [ AppComponent, AppRacingComponent, CarPartsComponent, MyRacesComponent ]
 })
